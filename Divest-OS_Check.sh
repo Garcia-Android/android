4627146676cade2aa1def4a1ac15e647732a3bd6
Alberto García <alberto@garcialnk.com>
2022-06-29 20:14:14 +0200

    cat md5sum

--- Divest-OS_Check.sh
+++ Divest-OS_Check.sh
@@ -3,7 +3,7 @@
 echo "OTA .zip"
 stat -c '%Y' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
 ls ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
-cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.sha512sum
+cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.md5sum
 stat -c '%s' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
 echo "recovery .img"
 ls ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-recovery.img
