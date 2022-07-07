7d9012f0a50ddaf53f96cf5accc44f0693845059
Alberto García <garcia98ftw@gmail.com>
2022-07-07 02:18:58 +0200

    Exit if directory is not found

--- Divest-OS_Build.sh
+++ Divest-OS_Build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 git pull;
-cd ~/android/DivestOS;
+cd ~/android/DivestOS || exit;
 git pull;
 cp Scripts/LineageOS-17.1/CVE_Patchers/android_kernel_xiaomi_sm6150.sh Scripts/LineageOS-19.1/CVE_Patchers/;
-cd Build/LineageOS-19.1;
+cd Build/LineageOS-19.1 || exit;
 source ../../Scripts/init.sh;
 resetWorkspace;
 patchWorkspace;
--- Divest-OS_Setup.sh
+++ Divest-OS_Setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p ~/android && cd ~/android;
+mkdir -p ~/android && cd ~/android || exit;
 git clone https://gitlab.com/divested-mobile/divestos-build.git DivestOS;
-cd DivestOS;
+cd DivestOS || exit;
 git submodule update --init --recursive; #must have git-lfs installed
 mkdir -p Build/LineageOS-19.1/.repo/local_manifests Builds;
 git apply ~/android/DivestOS-Build.patch;
-cd Build/LineageOS-19.1;
+cd Build/LineageOS-19.1 || exit;
 cat ../../Manifests/Manifest_LAOS-19.1.xml > .repo/local_manifests/local_manifest.xml;
 repo init -u https://github.com/LineageOS/android.git -b lineage-19.1;
 repo sync; #This part will take a while
