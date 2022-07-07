9a754affe50bcb34d585c813d8307ed7bdbc0c82
Alberto García <alberto@garcialnk.com>
2022-07-07 02:15:47 +0200

    Print json

--- Divest-OS_Check.sh
+++ Divest-OS_Check.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-echo "OTA .zip"
-stat -c '%Y' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
-ls ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
-cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.md5sum
-stat -c '%s' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip
-echo "recovery .img"
-ls ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-recovery.img
+echo "ota.zip"
+cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.sha512sum
+datetime=$(stat -c '%Y' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip)
+filename=$(find ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip -type f -exec basename {} \;)
+id=$(cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.md5sum)
+romtype="dos"
+size=$(stat -c '%s' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip)
+version="19.1"
+echo ""
+echo "recovery.img"
+cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.sha512sum
+echo ""
+echo "json"
+echo "{
+  \"response\": [
+    {
+      \"datetime\": ${datetime},
+      \"filename\": ${filename},
+      \"id\": ${id},
+      \"romtype\": ${romtype},
+      \"size\": ${size},
+      \"url\": \"\",
+      \"version\": ${version}
+    }
+  ]
+}"
