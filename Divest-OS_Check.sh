03902a7bf707831d57f50ac5e73e47607b9f383e
Alberto García <alberto@garcialnk.com>
2022-07-15 22:38:59 +0200

    Update Divest-OS_Check.sh

--- Divest-OS_Check.sh
+++ Divest-OS_Check.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo "ota.zip"
-cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.sha512sum
 datetime=$(stat -c '%Y' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip)
 filename=$(find ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip -type f -exec basename {} \;)
-id=$(cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.md5sum)
+id=$(cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.md5sum | cut -c -32)
 romtype="dos"
 size=$(stat -c '%s' ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip)
 version="19.1"
+echo "**SHA512 checksums:**"
 echo ""
-echo "recovery.img"
 cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-ota.zip.sha512sum
+echo ""
+cat ~/android/DivestOS/Builds/LineageOS-19.1/release_keys/divested-UNOFFICIAL-*-recovery.img.sha512sum
 echo ""
 echo "json"
 echo "{
   \"response\": [
     {
       \"datetime\": ${datetime},
-      \"filename\": ${filename},
-      \"id\": ${id},
-      \"romtype\": ${romtype},
+      \"filename\": \"${filename}\",
+      \"id\": \"${id}\",
+      \"romtype\": \"${romtype}\",
       \"size\": ${size},
       \"url\": \"\",
-      \"version\": ${version}
+      \"version\": \"${version}\"
     }
   ]
 }"
